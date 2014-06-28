3beddf21788205d9d9292a80d8e6949d248ca8fe
Kirin <Kirin@Kirin-PC>
2014-06-28 16:01:10 +0800

    first commit

---
+++ Test/src/main/java/com/surfm/report/WebSocketConfig.java
@@ -0,0 +1,35 @@
+package com.surfm.report;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableScheduling
+@ComponentScan("com.surfm.testws")
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Autowired Environment env;
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/chatWs").withSockJS();
+	}
+
+	
+	
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/message/");
+		registry.setApplicationDestinationPrefixes("/app");
+		
+	}
+
+}
